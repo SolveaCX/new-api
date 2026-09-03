d109d1f2d45472da32471bc40bac0ae6ed3cfd07
jilin zhang <jilin.zhang@shulex-tech.com>
2026-09-03 16:37:22 +0800

    fix: show complete user top-up history (#1085)
    * fix: record balance subscription purchases in billing history
    
    * fix: show complete user top-up history

--- model/topup_history_test.go
+++ model/topup_history_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestUserTopUpHistoryExcludesPendingAndExpiredOrders(t *testing.T) {
+func TestUserTopUpHistoryIncludesAllOrderStatuses(t *testing.T) {
 	setupTopUpLifecycleTestDB(t, 1)
 	require.NoError(t, DB.AutoMigrate(&PaymentInvoice{}))
 
@@ -33,13 +33,15 @@ func TestUserTopUpHistoryExcludesPendingAndExpiredOrders(t *testing.T) {
 	pageInfo := &common.PageInfo{Page: 1, PageSize: 10}
 	topUps, total, err := GetUserTopUps(user.Id, pageInfo)
 	require.NoError(t, err)
-	require.EqualValues(t, 2, total)
-	require.Len(t, topUps, 2)
+	require.EqualValues(t, 4, total)
+	require.Len(t, topUps, 4)
 	require.Equal(t, common.TopUpStatusSuccess, topUps[0].Status)
 	require.Equal(t, common.TopUpStatusFailed, topUps[1].Status)
+	require.Equal(t, common.TopUpStatusExpired, topUps[2].Status)
+	require.Equal(t, common.TopUpStatusPending, topUps[3].Status)
 }
 
-func TestSearchUserTopUpHistoryExcludesPendingAndExpiredOrders(t *testing.T) {
+func TestSearchUserTopUpHistoryIncludesAllOrderStatuses(t *testing.T) {
 	setupTopUpLifecycleTestDB(t, 1)
 	require.NoError(t, DB.AutoMigrate(&PaymentInvoice{}))
 
@@ -64,7 +66,43 @@ func TestSearchUserTopUpHistoryExcludesPendingAndExpiredOrders(t *testing.T) {
 	pageInfo := &common.PageInfo{Page: 1, PageSize: 10}
 	topUps, total, err := SearchUserTopUps(user.Id, "%searchable-history%", pageInfo)
 	require.NoError(t, err)
+	require.EqualValues(t, 3, total)
+	require.Len(t, topUps, 3)
+	require.Equal(t, common.TopUpStatusSuccess, topUps[0].Status)
+	require.Equal(t, common.TopUpStatusExpired, topUps[1].Status)
+	require.Equal(t, common.TopUpStatusPending, topUps[2].Status)
+}
+
+func TestUserTopUpHistoryIncludesRecordsOlderThanThirtyDays(t *testing.T) {
+	setupTopUpLifecycleTestDB(t, 1)
+	require.NoError(t, DB.AutoMigrate(&PaymentInvoice{}))
+
+	user := createLifecycleQuotaTestUser(t, "topup-history-long-term", 0, 100)
+	oldCreateTime := common.GetTimestamp() - 31*24*60*60
+	insertTopUpLifecycleOrder(
+		t,
+		user.Id,
+		"history-old-success",
+		PaymentProviderStripe,
+		common.TopUpStatusSuccess,
+		oldCreateTime,
+		oldCreateTime,
+	)
+
+	pageInfo := &common.PageInfo{Page: 1, PageSize: 10}
+	topUps, total, err := GetUserTopUps(user.Id, pageInfo)
+	require.NoError(t, err)
 	require.EqualValues(t, 1, total)
 	require.Len(t, topUps, 1)
-	require.Equal(t, common.TopUpStatusSuccess, topUps[0].Status)
+	require.Equal(t, "history-old-success", topUps[0].TradeNo)
+
+	searchResults, searchTotal, err := SearchUserTopUps(
+		user.Id,
+		"history-old-success",
+		pageInfo,
+	)
+	require.NoError(t, err)
+	require.EqualValues(t, 1, searchTotal)
+	require.Len(t, searchResults, 1)
+	require.Equal(t, "history-old-success", searchResults[0].TradeNo)
 }
